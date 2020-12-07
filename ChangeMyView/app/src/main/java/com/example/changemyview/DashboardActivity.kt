2f5ab47d55758895ae0e96b61f4b3a8066b1fc41
Chris Howard <choward4@umd.edu>
2020-12-07 17:15:44 -0500

    added icon and help menu. Updated html with icon

--- ChangeMyView/app/src/main/java/com/example/changemyview/DashboardActivity.kt
+++ ChangeMyView/app/src/main/java/com/example/changemyview/DashboardActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -40,9 +41,17 @@ class DashboardActivity : AppCompatActivity() {
     // Process clicks on Options Menu items
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.info -> {
+            R.id.report -> {
                 val intent = Intent(this@DashboardActivity, ReportUserActivity::class.java)
                 startActivity(intent)
+                true
+            } R.id.help -> {
+                AlertDialog.Builder(this)
+                    .setMessage("   In this application, you may select a controversial topic to be for or against. As soon as someone else marks an opposing viewpoint on your topic, " +
+                            "their email will appear in your matches. If you do not see any matches, then come visit the app the next day to check if you have been paired. \n\n   This is a " +
+                            "place to have your viewpoints on life challenged in a respectful way. If you are harassed by another user, please report them by using our report feature. " +
+                            "We will make sure to take appropriate action. \n\n   Have fun and keep an open mind!")
+                    .setCancelable(true).create().show()
                 true
             }
             else -> false
